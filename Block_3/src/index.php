120ec304cd5fb66470977c8a0946e298280ac02c
ilya vintovkin <vintovkinilya@gmail.com>
2025-04-06 20:48:39 +0700

    Block_3 task_1 - completed

---
+++ Block_3/src/index.php
@@ -0,0 +1,33 @@
+<?php
+enum Age: int {
+    case ZERO = 0;
+    case TWELVE = 12;
+    case THIRTY_FIVE = 35;
+    case SIXTY = 60;
+}
+
+enum Period: string {
+    case CHILDHOOD = "детство";
+    case YOUNG_ADULTHOOD = "молодость";
+    case OLD_AGE = "старость";
+}
+
+function getPeriodByAge(int $age): string {
+    return match (true) {
+        $age < Age::TWELVE->value => Period::CHILDHOOD->value,
+        $age < Age::THIRTY_FIVE->value => Period::YOUNG_ADULTHOOD->value,
+        default => Period::OLD_AGE->value,
+    };
+}
+
+$name = htmlspecialchars($_POST["name"]);
+$age = (int)$_POST["age"]; // обязательно приводим к числу
+
+if ($age <= Age::ZERO->value) {
+    echo "Возраст должен быть больше нуля.";
+    return;
+}
+
+$period = getPeriodByAge($age);
+
+echo "Привет, $name! Тебе $age лет, это — $period.";
